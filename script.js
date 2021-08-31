c37ecec2d987a7a248dd5edb4edbd42f07294df5
Govind Kumar <govind85050@gmail.com>
2021-08-31 23:44:47 +0530

    second Commit

--- script.js
+++ script.js
@@ -91,6 +91,9 @@ button.onclick = function () {
         setColor(temp)
         changeBoard(board)
     }
+    xhrRequest.open('get', 'https://sugoku.herokuapp.com/board?difficulty=easy')
+    //we can change the difficulty of the puzzle the allowed values of difficulty are easy, medium, hard and random
+    xhrRequest.send()
 }
 
 //to be completed by student
